57288722f17b0c0b63e515fa9940bb74755c8df6
kasatochkaa <62566487+kasatochkaa@users.noreply.github.com>
2020-04-25 00:06:05 +0400

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,14 +3,14 @@
 
 #include "Triangle.h"
 
-double Triangle::getPerimeter() {
+double Triangle::getPerimeter() const{
 
 	return p1.to(&p2) +
 		   p1.to(&p3) +
 		   p2.to(&p3);
 }
 
-double Triangle::getArea() {
+double Triangle::getArea() const {
 	/**
 	*  Площадь треугольника по координатам
 	* находим через определитель 2-го порядка:
@@ -20,17 +20,17 @@ double Triangle::getArea() {
 	*      |       | x2 - x3, y2 - y3 | |
 	**/
 
-	double x1 = p1.getX();
-	double x2 = p2.getX();
-	double x3 = p3.getX();
-	double y1 = p1.getY();
-	double y2 = p2.getY();
-	double y3 = p3.getY();
+	double x1 = p1.getX() const;
+	double x2 = p2.getX()const;
+	double x3 = p3.getX()const;
+	double y1 = p1.getY()const;
+	double y2 = p2.getY()const;
+	double y3 = p3.getY()const;
 
 	return abs( 0.5 * ((x1-x3)*(y2-y3) - (y1-y3)*(x2-x3)) );
 }
 
-void Triangle::print() {
+void Triangle::print()const {
 	std::cout << "Triangle" << std::endl;
 	std::cout << "P1: (" << p1.getX() << ", " << p1.getY() << ")" << std::endl;
 	std::cout << "P2: (" << p2.getX() << ", " << p2.getY() << ")" << std::endl;
@@ -39,7 +39,7 @@ void Triangle::print() {
 	std::cout << "Area: " << getArea() << std::endl;
 }
 
-Point Triangle::getP1() {
+Point Triangle::getP1()const {
 	return this->p1;
 }
 
@@ -47,7 +47,7 @@ void Triangle::setP1( const Point p) {
 	this->p1 = p;
 }
 
-Point Triangle::getP2() {
+Point Triangle::getP2() const {
 	return this->p2;
 }
 
@@ -55,7 +55,7 @@ void Triangle::setP2(const Point p) {
 	this->p2 = p;
 }
 
-Point Triangle::getP3() {
+Point Triangle::getP3()const {
 	return this->p3;
 }
 
