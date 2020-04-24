fcd905e27e8a6459027010c290b131ea920ee628
kasatochkaa <62566487+kasatochkaa@users.noreply.github.com>
2020-04-25 00:06:58 +0400

    Update Triangle.h

--- Triangle.h
+++ Triangle.h
@@ -19,19 +19,19 @@ public:
 		const Point val3) : p1(val1), p2(val2), p3(val3) {}
 	Triangle(const Triangle &copy) : p1(copy.p1), p2(copy.p2), p3(copy.p3) {}
 
-	Point getP1();
+	Point getP1()const;
 	void setP1(const Point p);
 
-	Point getP2();
+	Point getP2()const;
 	void setP2(const Point p);
 
-	Point getP3();
+	Point getP3()const;
 	void setP3(const Point p);
 
-	void print();
+	void print()const;
 
-	double getPerimeter() override;
-	double getArea() override;
+	double getPerimeter() override const;
+	double getArea() override const;
 
 	Triangle& operator= (const Triangle &obj);
 
